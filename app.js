b0302a005c693b77ea34e73d123f77ab9fb24f6f
plocesar <136383343+plocesar@users.noreply.github.com>
2025-09-03 23:08:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -86,3 +86,33 @@ if (canvas) {
     console.log(dataURL);
   }
 }
+
+
+
+function exportPDF() {
+  const element = document.body;
+  html2canvas(element, { scale: 2 }).then(canvas => {
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jspdf.jsPDF("p", "mm", "a4");
+
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.getHeight();
+    const imgWidth = pageWidth;
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+
+    while (heightLeft > 0) {
+      position = heightLeft - imgHeight;
+      pdf.addPage();
+      pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+    }
+
+    pdf.save("checklist_vistoria.pdf");
+  });
+}
